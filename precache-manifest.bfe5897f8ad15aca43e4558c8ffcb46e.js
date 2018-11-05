2fe49902506f936dad2170ec815d9d745e055d70
Alan Uraz <alanuraz@gmail.com>
2018-11-05 12:23:51 -0500

    Updates

---
+++ precache-manifest.bfe5897f8ad15aca43e4558c8ffcb46e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "ba0cb1b08b61e8343747",
+    "url": "/todo/static/js/main.ba0cb1b0.chunk.js"
+  },
+  {
+    "revision": "910c9652af037f81b001",
+    "url": "/todo/static/js/1.910c9652.chunk.js"
+  },
+  {
+    "revision": "ba0cb1b08b61e8343747",
+    "url": "/todo/static/css/main.88165565.chunk.css"
+  },
+  {
+    "revision": "335298ab2ae18d2cbd7c166bf75c9b6b",
+    "url": "/todo/index.html"
+  }
+];
